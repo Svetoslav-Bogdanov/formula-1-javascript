26bb34acb34115cc46a3a6eae62c16fed73578cf
slavi <shimmy@abv.bg>
2022-04-18 20:16:18 +0300

    SSchedule html and javascript

--- schedule.js
+++ schedule.js
@@ -3,5 +3,24 @@ window.onload = () =>{
     
     var season = urlParams.get("season");
 
+    listSeason(season);
+
     console.log(season);
+}
+
+function listSeason(selectedSeason) {
+    fetch("https://ergast.com/api/f1/{{year}}.json".replace("{{year}}", selectedSeason))
+    .then(result => result.json())
+    .then(result => buildScheduleList(result.MRData.RaceTable.Races));
+
+}
+
+function buildScheduleList (schedule){
+    let scheduleList = "";
+
+    schedule.forEach(element => {
+        scheduleList += ``
+    });
+
+    console.log(schedule);
 }
